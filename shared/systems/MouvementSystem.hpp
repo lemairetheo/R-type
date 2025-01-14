73e762bf4714cbbea01ee86b778674426f776c4d
Romain <114572787+romaincollignon@users.noreply.github.com>
2025-01-14 17:11:00 +0000

    feat(server): add block players/Enemies and auto move enemies

--- shared/systems/MouvementSystem.hpp
+++ shared/systems/MouvementSystem.hpp
@@ -5,14 +5,31 @@ namespace rtype {
     class MovementSystem : public ISystem {
     public:
         void update(EntityManager& manager, float dt) override {
+            auto walls = manager.getEntitiesWithComponents<Wall>();
+
             for (EntityID entity = 0; entity < MAX_ENTITIES; ++entity) {
                 if (manager.hasComponent<Position>(entity) && manager.hasComponent<Velocity>(entity)) {
                     auto& pos = manager.getComponent<Position>(entity);
                     auto& vel = manager.getComponent<Velocity>(entity);
                     auto& proj = manager.getComponent<Projectile>(entity);
 
-                    pos.x += vel.dx * dt;
-                    pos.y += vel.dy * dt;
+                    if (manager.hasComponent<Projectile>(entity)) {
+                        pos.x += vel.dx * dt;
+                        pos.y += vel.dy * dt;
+                    } else {
+                        for (EntityID wall : walls) {
+                            const auto& wallPos = manager.getComponent<Position>(wall);
+
+                            if (manager.hasComponent<Enemy>(entity) && checkCollisionRect({pos.x + (vel.dx * dt), pos.y + (vel.dy * dt)}, 25.0f, wallPos, 40.0f, 80.0f))
+                                pos.y += 50 * dt;
+                            if (!checkCollisionRect({pos.x + (vel.dx * dt), pos.y + (vel.dy * dt)}, 13.0f, wallPos, 20.0f, 60.0f)) {
+                                pos.x += vel.dx * dt;
+                                pos.y += vel.dy * dt;
+                                break;
+                            }
+                        }
+                    }
+
 
                     if (manager.hasComponent<Projectile>(entity)) {
                         if (pos.x > 795)
@@ -25,6 +42,16 @@ namespace rtype {
                     }
                 }
             }
+        }
+
+        bool checkCollisionRect(const Position& circlePos, float radius, const Position& rectPos, float rectWidth, float rectHeight) {
+            float closestX = std::max(rectPos.x, std::min(circlePos.x, rectPos.x + rectWidth));
+            float closestY = std::max(rectPos.y, std::min(circlePos.y, rectPos.y + rectHeight));
+
+            float dx = circlePos.x - closestX;
+            float dy = circlePos.y - closestY;
+
+            return (dx * dx + dy * dy) <= (radius * radius);
         }
     };
 }
